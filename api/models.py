87dc6f51438527d27823d51509f5493ba55e7948
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2024-11-05 16:20:27 -0600

    UPDATE MODEL

--- api/models.py
+++ api/models.py
@@ -75,32 +75,3 @@ class Usuarios(AbstractUser):
     @property
     def is_superuser(self):
         return self.rol.is_superuser
-
-
-
-
-class ChatMessage(models.Model):
-    user = models.ForeignKey(Usuarios, on_delete=models.SET_NULL, null=True, related_name="user")
-    sender = models.ForeignKey(Usuarios, on_delete=models.SET_NULL, null=True, related_name="sender")
-    reciever = models.ForeignKey(Usuarios, on_delete=models.SET_NULL, null=True, related_name="reciever")
-
-    message = models.TextField()
-
-    is_read = models.BooleanField(default=False)
-    date = models.DateTimeField(auto_now_add=True)
-    
-    class Meta:
-        ordering = ['date']
-        verbose_name_plural = "Message"
-
-    def __str__(self):
-        return f"{self.sender} - {self.reciever}"
-
-    @property
-    def sender_profile(self):
-        sender_profile = UserManager.objects.get(user=self.sender)
-        return sender_profile
-    @property
-    def reciever_profile(self):
-        reciever_profile = UserManager.objects.get(user=self.reciever)
-        return reciever_profile
